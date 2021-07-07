19f2ae4b10e08393722ac39fb800a4e08ef119b9
keith72632 <keithhorton30@outlook.com>
2021-07-07 07:21:04 -0500

    SPI_Init

--- Spi/stm32_spi_drivers/drivers/inc/stm32f407xx_spi.h
+++ Spi/stm32_spi_drivers/drivers/inc/stm32f407xx_spi.h
@@ -59,6 +59,37 @@ typedef struct
     SPI_Config_t    SPIConfig;
 }SPI_Handle_t;
 
+#define SPI_DEVICE_MODE_MASTER 1
+#define SPI_DEVICE_MODE_SLAVE  0
+
+//Spi Bus Conifg. Full-duplex, Half-duplex and Simplex
+#define SPI_BUS_CONFIG_FD      1
+#define SPI_BUS_CONFIG_HD      2
+#define SPI_BUS_CONFIG_RX      3
+
+//SPI Clock Speed. In CR1. Prescaler
+#define SPI_SCLK_SPEED_DIV2    0
+#define SPI_SCLK_SPEED_DIV4    1
+#define SPI_SCLK_SPEED_DIV8    2
+#define SPI_SCLK_SPEED_DIV16   3
+#define SPI_SCLK_SPEED_DIV32   4
+#define SPI_SCLK_SPEED_DIV64   5
+
+//DFF
+#define SPI_DFF_8BITS          0
+#define SPI_DFF_16BITS         1
+
+//CPOL
+#define SPI_CPOL_HIGH          1
+#define SPI_CPOL_LOW           0
+
+//CPHA
+#define SPI_CPHA_HIGH          1
+#define SPI_CPHA_LOW           0
+
+//SSM
+#define SPI_SSM_EN             1
+#define SPI_SSM_DI             0
 /**************************************************************************************************
  *                                      API Prototypes                                            *
  *************************************************************************************************/
--- Spi/stm32_spi_drivers/drivers/src/spi.c
+++ Spi/stm32_spi_drivers/drivers/src/spi.c
@@ -36,7 +36,45 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 
 void SPI_Init(SPI_Handle_t *pSPIHandle)
 {
+	//Events shown by SR(status register)
+	//Master produces clock
+	//SCLK is only configured when master is transmitting data
+	uint32_t tempreg = 0;
 
+	//1.Configure device mode
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR; //bit 2
+
+	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		//bidi mode should be cleared
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE); //bit 15
+	}
+	else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		//bidi mode should be set
+		tempreg |= (1 << SPI_CR1_BIDIMODE); //bit 15
+	}
+	else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_RX)
+	{
+		//BIDI mode should be cleared
+		//RXONLY bit must be set
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE); //bit 15
+		tempreg |= (1 << SPI_CR1_RXONLY); //bit 10
+	}
+
+	//3. Configure the spi serial clock speed (baud rate)
+	tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR; //bit 3
+
+	//4. Configure the DFF
+	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF; //bit 11
+
+	//5. Configure the CPOL
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL; //bit 1
+
+	//6. Configure the CPHA
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA; //bit 0
+
+	pSPIHandle->pSPIx->CR1 = tempreg;
 };
 
 void SPI_DeInit(SPI_RegDef_t *pSPIx)
